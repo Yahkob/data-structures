e13aa8a5f473d03b860b04a5f1a06ca61f709aa5
Hack Reactor Students <jakobharclerode@gmail.com>
2014-06-05 11:28:23 -0700

    finished part 1 data sprint

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._size = 0;
 };
 
+
+  Stack.prototype.push = function(value){
+    this._size++;
+    this[this._size] = value;
+  };
+
+  Stack.prototype.pop = function(){
+    var temp = this[this._size]
+    delete this[this._size];
+    if(this._size !== 0){
+      this._size--;
+    }
+    return temp;
+  };
+
+  Stack.prototype.size = function(){
+    return this._size;
+  };
 
